3b054d25cccbcf121ee83c884d95a7e5f92fdbe8
Ivan Meredith <ivan@ivan.net.nz>
2010-05-08 12:57:29 +1200

    Acess keys now work without the modifier

--- question.js
+++ question.js
@@ -1,6 +1,49 @@
 var form = $('form[action=answer.php]');
+var getNextPage = false;
+var nextPageData = null;
+
+$('input[name=showanswer]').click(function (e){
+   e.preventDefault();
+   if(nextPageData){
+       nextPageData();
+   } else {
+       getNextPage=true;
+   }
+});
+
 $(document).keypress(function(e){
-    if(e.which == 32){
-        form.submit();
+    var charStr = String.fromCharCode(e.which);
+     
+    if(charStr == 'f' || charStr == '8' ){
+        if(nextPageData){
+            nextPageData();
+        } else {
+            getNextPage=true;
+        }
+        return;
+    }
+    
+    $('[accesskey='+charStr+']').click();
+    
+});
+
+$.get('answer.php', function(data){
+    if(getNextPage){
+        $('body').html(data);
+        setupAnswer();
+    } else {
+        nextPageData = function () {
+            $('body').html(data);
+            setupAnswer();
+        };
     }
 });
+
+function setupAnswer(){
+    $(document).keypress(function(e){
+        var charStr = String.fromCharCode(e.which);
+
+        $('[accesskey='+charStr+']').click();
+
+    });
+}
